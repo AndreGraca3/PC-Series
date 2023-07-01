7be5d42254e16fab49117691c822300a4465d319
sindr <sindrome.gracinha@gmail.com>
2023-07-01 18:29:12 +0100

    Future enhancements.

--- src/main/kotlin/pt/isel/pc/problemsets/set1/Future.kt
+++ src/main/kotlin/pt/isel/pc/problemsets/set1/Future.kt
@@ -2,21 +2,20 @@ package pt.isel.pc.problemsets.set1
 
 import java.util.concurrent.CancellationException
 import java.util.concurrent.ExecutionException
+import java.util.concurrent.Future
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
 import kotlin.jvm.Throws
 
-class Future<T> {
-
+class Future<T> : Future<T> {
     private val lock = ReentrantLock()
     private val waiters = lock.newCondition()
 
     private var thread: Thread? = null
     private var result: T? = null
     private var exception: Exception? = null
-    private var isDone = false
     private var isCancelled = false
 
     fun start() {
@@ -25,62 +24,77 @@ class Future<T> {
 
     fun setResult(result: T) {
         lock.withLock {
-            if (!isDone) {
-                this.result = result
-                isDone = true
-                waiters.signalAll()
-            }
+            if (isDone) return
+            this.result = result
+            waiters.signalAll()
         }
     }
 
     fun setException(e: Exception) {
         lock.withLock {
-            if (!isDone) {
-                this.exception = e
-                isDone = true
-                waiters.signalAll()
-            }
+            if (isDone) return
+            this.exception = e
+            waiters.signalAll()
         }
     }
 
-    @Throws(ExecutionException::class)
-    fun get(): T? {
-        lock.withLock {
-            while (true) {
-                if (isCancelled) throw CancellationException()
-                if (exception != null) throw ExecutionException(exception)
-                if (isDone) return result
-                waiters.await()
-            }
-        }
+    override fun get(): T {
+        return getWithCtx()
     }
 
     @Throws(TimeoutException::class)
-    fun get(timeout: Long, unit: TimeUnit): T? {
+    override fun get(timeout: Long, unit: TimeUnit): T {
+        return getWithCtx(timeout, unit)
+    }
+
+    private fun getWithCtx(timeout: Long? = null, unit: TimeUnit? = null): T {
         lock.withLock {
-            if (isCancelled) throw CancellationException()
-            if (exception != null) throw ExecutionException(exception)
-            if (isDone) return result
-            waiters.await(timeout, unit)
-            if (isCancelled) throw CancellationException()
-            if (exception != null) throw ExecutionException(exception)
-            if (!isDone) throw TimeoutException()
-            return result
+            // fast path
+            var res = getResult()
+            if(res != null) return res
+
+            // wait path
+            while (true) {
+                try {
+                    if (timeout == null) waiters.await()
+                    else waiters.await(timeout, unit)
+                } catch (e: InterruptedException) {
+                    res = getResult()
+                    if(res != null) {
+                        Thread.currentThread().interrupt()
+                        return res
+                    }
+                    throw e
+                }
+                return getResult() ?: throw TimeoutException("Timeout reached.")
+            }
         }
     }
 
-    fun cancel(mayInterruptIfRunning: Boolean) {
+    // Gets current result, throws Exception if this future is cancelled or ended with exception.
+    private fun getResult(): T? {
+        if (isCancelled) throw CancellationException()
+        if (exception != null) throw ExecutionException(exception)
+        return result
+    }
+
+    override fun cancel(mayInterruptIfRunning: Boolean): Boolean {
         lock.withLock {
+            if (isCancelled) return false
             isCancelled = true
-            isDone = true
             waiters.signalAll()
             if (mayInterruptIfRunning) thread?.interrupt()
+            return true
         }
     }
 
-    fun isDone(): Boolean {
+    override fun isCancelled(): Boolean {
+        lock.withLock { return isCancelled }
+    }
+
+    override fun isDone(): Boolean {
         lock.withLock {
-            return isDone
+            return result != null || exception != null
         }
     }
 }
